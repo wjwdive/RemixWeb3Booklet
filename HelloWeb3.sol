e9b43f72c2cb26fad8b6c423eb115452cc443fa1
wjwdive <wjwdive@163.com>
2025-08-15 22:17:43 +0800

    init

---
+++ HelloWeb3.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract HelloWeb3 {
+    string public _string = "Hello Web3!";
+    int public aInt = -1;
+    uint public aUint = 0;
+    uint256 public aNumber = 2025;
+}   
